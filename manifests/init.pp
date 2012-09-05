405312b1329dad7fe61fd6ec3853278e40fd98d6
Guzmán Brasó <guzman@guruhub.com.uy>
2012-09-05 17:03:32 -0300

    doc moved to readme

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,3 @@
-#
-# Class to install Nullsoft Software installer version 2.46 plus osslsigncode version 1.4
-#
-# OS Fully Tested: Debian Squeeze with puppet 2.7 from backports.
-#
-# Arguments:
-# root_folder 		=> Full path to folder where all sourcecode will be downloaded, uncompressed and compiled. 
-# make_public 		=> True or False - Create symlinks for binaries that did not create themselves, default true.
-#
-# Requires wget module from https://github.com/liquidstate/puppet-wget
-#
-# Based on linux install guide by John Mendez ( http://www.xdevsoftware.com/blog/post/How-to-Install-the-Nullsoft-Installer-NSIS-on-Linux-.aspx )
-#
-# Code by Guzman Braso - guruHub - www.guruhub.com.uy
-#
 class nsis($root_folder='/usr/local/src',$make_public=true) {
 
 	# Include wget so we can use it to download latest tar.gz from nsis official website.
